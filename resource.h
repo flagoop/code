d811482da9ac902bc1b271f37db39bbf933c886f
zhulin <406171450@qq.com>
2017-06-14 16:57:16 +0800

    Delete resource.h

--- resource.h
+++
@@ -1,7 +0,0 @@
-#ifndef IDC_STATIC
-#define IDC_STATIC (-1)
-#endif
-
-#define IDR_MENU1                               100
-#define IDM_FILE_EXIT                           40000
-#define IDM_FILE_OPEN                           40001
